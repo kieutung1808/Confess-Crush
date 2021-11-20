e22064771b3c022eab86fad99bd4e11e6dbe23c3
kieutung1808 <94726667+kieutung1808@users.noreply.github.com>
2021-11-20 09:08:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,20 @@
 const CONFIG = {
-    titleWeb: "❤️ Gửi Thị Hiền",
-    introTitle: 'NGUYỄN THỊ HIỀN',
-    introDesc: 'Chúc mày 20-11 vui vẻ, ngày càng chăm chỉ và gặp nhiều may mắn nhé😘',
-    btnIntro: '^^Chuyển slide đi m^^',
-    title: 'YÊU TAO KHÔNG ?',
-    desc: 'Tắt cái này đi mà không trả lời chứng tỏ yêu tao',
-    btnYes: 'YES! TỚI LUÔN',
-    btnNo: 'ĐÉO CÚT LUÔN',
+    titleWeb: "Tiêu đề web",
+    introTitle: 'Tên người yêu bạn',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '^^HiHi^^',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi câu trả lời',
-    reply: 'Đơn giản vì mày đẹp trai',
-    mess: 'Tao biết mà 👫. Yêu Mày nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, tao qua đón đi chơi nha.',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://scontent.fhan3-5.fna.fbcdn.net/v/t1.15752-9/251999517_1486252638423881_4728195695983057861_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=ae9488&_nc_ohc=pYBOrO7HsOUAX8Su3bL&_nc_ht=scontent.fhan3-5.fna&oh=0366c77cf8997ba6ce3dc83820202eac&oe=61BCB2A0' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
